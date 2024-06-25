080daa8de0a0b73ca6214dd5c13ae68f4e5a05cf
drewsdodger <67015438+drewsdodger@users.noreply.github.com>
2024-06-25 20:42:48 +0100

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Social Media App</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
